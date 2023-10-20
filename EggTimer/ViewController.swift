5526d1d01530c5b1b042bd6f3955610f0dfa342b
Dinar Shakirov <139841080+bonycl@users.noreply.github.com>
2023-10-20 20:42:06 +0700

    setup UI

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,61 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
     
-
-
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    var timer = Timer ()
+    var totalTime = 0
+    var secondsPassed = 0
+    var player: AVAudioPlayer?
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        //progressBar.progress = 1.0
+        totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self] (Timer) in
+            if secondsPassed < totalTime  {
+                secondsPassed += 1
+                let percentageProgress = Float(secondsPassed) / Float(totalTime)
+                //print(percentageProgress)
+                progressBar.progress = percentageProgress
+                //print(percentageProgress)
+                
+            } else {
+                timer.invalidate()
+                titleLabel.text = "Done!"
+                playSound(soundName: "alarm_sound")
+                print("Done!")
+            }
+        }
+    }
+    
+    func playSound(soundName: String) {
+        guard let path = Bundle.main.path(forResource: soundName, ofType:"mp3") else {
+            return }
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
 }
+
